255bba8eb050afc55e6ec155cfb24822e71d00d8
tea <bluetakufr@gmail.com>
2024-06-27 00:53:45 +0200

    fix: setCookie

--- src/routes/proxy/+server.ts
+++ src/routes/proxy/+server.ts
@@ -1,10 +1,19 @@
-export const POST = async ({ request }) => {
+import { json } from '@sveltejs/kit'
+
+export const POST = async ({ request, fetch }) => {
     const { url, headers, method, body } = await request.json()
     const response = await fetch(url, {
         headers,
         method,
         body: body ? JSON.stringify(body) : undefined,
     })
-    response.headers.set('set-cookie', response.headers.getSetCookie().join('\n'))
-    return response
+
+    return json({
+        status: response.status,
+        statusText: response.statusText,
+        ok: response.ok,
+        headers: new Object(response.headers.entries()),
+        body: await response.text(),
+        setCookie: response.headers.getSetCookie(),
+    })
 }
